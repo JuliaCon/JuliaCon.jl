4f15f18b729a8113b2bb25d152efb89760622bd3
Carsten Bauer <crstnbr@gmail.com>
2021-05-30 17:05:37 +0200

    juliacon2021()

--- src/JuliaCon.jl
+++ src/JuliaCon.jl
@@ -1,5 +1,7 @@
 module JuliaCon
 
-# Write your package code here.
+juliacon2021() = println("Welcome to JuliaCon 2021! Find more information on https://juliacon.org/2021/.")
+
+export juliacon2021
 
 end
