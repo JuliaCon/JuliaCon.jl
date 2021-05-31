75f246501716fad2992d9a270c977f229696717e
Carsten Bauer <crstnbr@gmail.com>
2021-05-31 09:47:37 +0200

    fix merge

--- src/JuliaCon.jl
+++ src/JuliaCon.jl
@@ -2,7 +2,13 @@ module JuliaCon
 
 using Requires
 
-juliacon2021() = println("Welcome to JuliaCon 2021! Find more information on https://juliacon.org/2021/.")
+include("countries.jl")
+
+function juliacon2021()
+    return println(
+        "Welcome to JuliaCon 2021! Find more information on https://juliacon.org/2021/."
+    )
+end
 
 function __init__()
     @require Distributed="8ba89e20-285c-5b6f-9357-94700520ee1b" @eval Main @everywhere using JuliaCon
