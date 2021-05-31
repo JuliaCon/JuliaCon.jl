46bd4d959f25ab1cad0db49ee5a79136a1b87e1e
Carsten Bauer <crstnbr@gmail.com>
2021-05-31 22:02:54 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using JuliaCon
 using Test
+using Distributed
 
 @testset "JuliaCon.jl" begin
-    # Write your tests here.
+    @testset "juliacon2021()" begin
+        @info "Local:"
+        @test isnothing(juliacon2021())
+
+        @info "Distributed:"
+        withenv("JULIA_PROJECT"=>pwd()) do
+            addprocs(4)
+            @eval Main @everywhere using JuliaCon
+            @test isnothing(@everywhere juliacon2021())
+            rmprocs(workers())
+        end
+    end
 end
