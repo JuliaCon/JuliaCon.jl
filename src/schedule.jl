9503947c4a6d6737d35adc88c63535eb93420c50
roflmaostc <fxw+git@mailbox.org>
2021-07-28 12:57:47 +0200

    Refactor methods like today and talks_by

--- src/schedule.jl
+++ src/schedule.jl
@@ -373,87 +373,19 @@ function today(::Val{:terminal}; now, speaker, track, terminal_links, highlighti
     tracks, tables, highlighters = _get_today_tables(; now, speaker, track, terminal_links, highlighting)
     isnothing(tables) && return nothing
 
-    header = (["Time", "Title", "Type", "Speaker"],)
-    header_crayon = crayon"dark_gray bold"
-    border_crayon = crayon"dark_gray"
-    h_times = Highlighter((data, i, j) -> j == 1, crayon"white bold")
-
-    println()
-    println(Dates.format(TimeZones.Date(now), "E d U Y"))
-
-    for j in eachindex(tracks)
-        track = tracks[j]
-        data = tables[j]
-        h_running = highlighters[j]
-        println()
-        pretty_table(
-            data;
-            title=track,
-            title_crayon=Crayon(; foreground=_track2color(track), bold=true),
-            header=header,
-            header_crayon=header_crayon,
-            border_crayon=border_crayon,
-            highlighters=(h_times, h_running),
-            tf=tf_unicode_rounded,
-            alignment=[:c, :l, :c, :l],
-        )
-    end
-
-    if legend 
-        print_legend(highlighting)
-    end
+    pretty_print_results(now, tracks, legend, highlighting, tables, highlighters)
     return nothing
 end
+
+
 
 function today(::Val{:text}; now, speaker, track, terminal_links, highlighting=true, legend=true)
     tracks, tables, highlighters = _get_today_tables(; now, speaker, track, terminal_links, highlighting, text_highlighting=highlighting)
     isnothing(tables) && return nothing
 
-    header = (["Time", "Title", "Type", "Speaker"],)
-    header_crayon = crayon"dark_gray bold"
-    border_crayon = crayon"dark_gray"
-    h_times = Highlighter((data, i, j) -> j == 1, crayon"white bold")
-
-    strings = Vector{String}()
-    push!(strings, string(Dates.format(TimeZones.Date(now), "E d U Y")))
-    for j in eachindex(tracks)
-        track = tracks[j]
-        data = tables[j]
-        h_running = highlighters[j]
-        str = pretty_table(
-            String,
-            data;
-            title=track,
-            title_crayon=Crayon(; foreground=_track2color(track), bold=true),
-            header=header,
-            header_crayon=header_crayon,
-            border_crayon=border_crayon,
-            highlighters=(h_times, h_running),
-            tf=tf_unicode_rounded,
-            alignment=[:c, :l, :c, :l],
-        )
-        push!(strings, str)
-    end
-
-    legend = if highlighting
-        """
-        Currently running talks are prefixed by a '>'.
-
-        """
-    else
-        ""
-    end
-
-    legend *= """
-    $(JuliaCon.abbrev(JuliaCon.Talk)) = Talk, $(JuliaCon.abbrev(JuliaCon.LightningTalk)) = Lightning Talk, $(JuliaCon.abbrev(JuliaCon.SponsorTalk)) = Sponsor Talk, $(JuliaCon.abbrev(JuliaCon.Keynote)) = Keynote,
-    $(JuliaCon.abbrev(JuliaCon.Workshop)) = Workshop, $(JuliaCon.abbrev(JuliaCon.Minisymposium)) = Minisymposium, $(JuliaCon.abbrev(JuliaCon.BoF)) = Birds of Feather,
-    $(JuliaCon.abbrev(JuliaCon.Experience)) = Experience, $(JuliaCon.abbrev(JuliaCon.VirtualPoster)) = Virtual Poster
-
-    Check out $(CONFERENCE_SCHEDULE_URL) for more information.
-    """
-    push!(strings, legend)
-    return strings
+    results_to_string(now, tracks, legend, highlighting, tables, highlighters)
 end
+
 
 """     tomorrow(; speaker)
 
@@ -485,10 +417,12 @@ function talks_by(speaker; legend=false)
 
     # list of ZoneDateTime for all JuliaCon days
     all_juliacon_dates = map(d -> ZonedDateTime(d..., JuliaCon.LOCAL_TIMEZONE), days)
-    t(d, legend) = today(; now = d, speaker, legend)
+    t(d, legend) = _get_today_tables(; now = d, speaker, legend)
 
     for d in all_juliacon_dates
-        t(d, legend)
+        tracks, tables, highlighters = _get_today_tables(; now=d, speaker)
+        isnothing(tables) && continue
+        pretty_print_results(d, tracks, legend, true, tables, highlighters)
     end
 
     return nothing
